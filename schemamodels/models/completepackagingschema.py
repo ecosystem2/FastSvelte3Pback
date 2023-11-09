6eb3e469ea766ba8a0d74b820c852aa30782d2ac
ecosystem2 <65734159+ecosystem2@users.noreply.github.com>
2023-11-09 16:50:18 +0000

    schema rework column headings and formats

--- schemamodels/models/completepackagingschema.py
+++ schemamodels/models/completepackagingschema.py
@@ -1,36 +1,78 @@
 import pandera as pa
+import pandas as pd
 from pandera.typing import Series
 from typing import Optional
+import uuid
 
 # Define a regular expression to match ISO 8601 date format "YYYY-MM-DD"
 iso8601_date_pattern = r"^\d{4}-\d{2}-\d{2}$"
 
-# Define the controlled list of valid items
-controlled_list = [
-    "bm-material-type-0001", "bm-material-type-0002", "bm-material-type-0003", "bm-material-type-0004", "bm-material-type-0005"]
+# Load the CSV file as a controlled list
+controlled_list_df = pd.read_csv(
+    'schemamodels\models\controlled_lists\list_of_lists_nov23.csv')
+# Replace 'column_name' with the actual column name in your CSV
+product_type_controlled_list = controlled_list_df['product type'].tolist()
+deposit_return_scheme_controlled_list = controlled_list_df['deposit return scheme'].tolist(
+)
+
+# Custom check function for UUID4 format
+# previous check was pa.Check.str_length(36),
+
+
+def check_uuid4(value):
+    try:
+        uuid_obj = uuid.UUID(value, version=4)
+        return True
+    except ValueError:
+        return False
 
 
 # import column from pandera as pa.column
 schema = pa.DataFrameSchema(
     {
-        "identifier": pa.Column(str),
-        "baseMaterialName": pa.Column(str),
-        "baseMaterialType": pa.Column(pa.String, checks=pa.Check(lambda s: s.isin(controlled_list))),
-        "materialChemCID": pa.Column(int),
-        "externalIdentifierKeys": pa.Column(str),
-        "externalIdentifierValues": pa.Column(int),
-        "certification": pa.Column(bool),
-        "certificationClaims": pa.Column(str, nullable=True),
-        "manufacturer": pa.Column(str),
-        "manufacturedCountry": pa.Column(str),
+        "identifier": pa.Column(str, checks=pa.Check(lambda s: check_uuid4(s))),
+        "completePackagingName": pa.Column(str, required=False),
+        "description": pa.Column(str, required=False),
+        "externalIdentifier": pa.Column(dict, required=False),
+        "imageURLs": pa.Column(list, required=False),
+        "completePackagingConstituentsIdentifier": pa.Column(list),
+        "LOWcodeWOproduct": pa.Column(str, required=False),
+        "productType": pa.Column(pa.String, checks=pa.Check(lambda s: s.isin(product_type_controlled_list)), required=False),
+        "componentContactWithProduct": pa.Column(list),
+        "LOWcodeWproduct": pa.Column(str, required=False),
+        "onTheGo": pa.Column(bool),
+        "householdWaste": pa.Column(bool),
+        "depositReturnSchemes": pa.Column(pa.String, checks=pa.Check(lambda s: s.isin(deposit_return_scheme_controlled_list))),
+        "completePackagingEndOfLifeRoutes": pa.Column(list, required=False),
+        "recyclability": pa.Column(bool, required=False),
+        "recyclabilityClaims": pa.Column(list, required=False),
+        "height": pa.Column(int, required=False),
+        "heightDate": pa.Column(pa.String, checks=pa.Check.str_matches(iso8601_date_pattern), required=False),
+        "width": pa.Column(int, required=False),
+        "widthDate": pa.Column(pa.String, checks=pa.Check.str_matches(iso8601_date_pattern), required=False),
+        "depth": pa.Column(int, required=False),
+        "depthDate": pa.Column(pa.String, checks=pa.Check.str_matches(iso8601_date_pattern), required=False),
+        "volume": pa.Column(int, required=False),
+        "volumeDate": pa.Column(pa.String, checks=pa.Check.str_matches(iso8601_date_pattern), required=False),
+        "weight": pa.Column(int),
+        "weightTolerance": pa.Column(int),
+        "weightToleranceType": pa.Column(str),
+        "weightDate": pa.Column(pa.String, checks=pa.Check.str_matches(iso8601_date_pattern), required=False),
+        "servingCapacity": pa.Column(int, required=False),
+        "servingCapacityDate": pa.Column(pa.String, checks=pa.Check.str_matches(iso8601_date_pattern), required=False),
+        "partOfMultipack": pa.Column(bool),
+        "certification": pa.Column(bool, required=False),
+        "certificationClaims": pa.Column(list, required=False),
+        "manufacturedCountry": pa.Column(int, required=False),
         "updateDate": pa.Column(pa.String, checks=pa.Check.str_matches(iso8601_date_pattern)),
+        "releaseDate": pa.Column(pa.String, checks=pa.Check.str_matches(iso8601_date_pattern), required=False),
+        "discontinueDate": pa.Column(pa.String, checks=pa.Check.str_matches(iso8601_date_pattern), required=False),
     },
     strict=True,
     coerce=True
 )
 
 
-# Define a function to validate and log errors
 def validate_and_log_data(data):
     log_filename = 'validation_errors.log'
     with open(log_filename, 'w') as log_file:
@@ -42,10 +84,10 @@ def validate_and_log_data(data):
         except pa.errors.SchemaErrors as err:
             with open(log_filename, 'a') as log_file:
                 log_file.write(f"Error #{row_index}:\n")
-                for error_index, error_row in enumerate(err.failure_cases.iterrows(), start=1):
-                    log_file.write(f"Error #{error_index}:\n")
-                    for col_name, col_error in error_row[1].items():
-                        log_file.write(f"{col_name}: {col_error}\n")
+                log_file.write("Schema errors and failure cases:\n")
+                log_file.write(f"{err.failure_cases.to_string()}\n")
+                log_file.write("\nDataFrame object that failed validation:\n")
+                log_file.write(f"{err.data.to_string()}\n")
                 log_file.write("\n")  # Add a separator between rows
 
 
--- schemamodels/models/materialschema.py
+++ schemamodels/models/materialschema.py
@@ -2,6 +2,7 @@ import pandera as pa
 import pandas as pd
 from pandera.typing import Series
 from typing import Optional
+import uuid
 
 # Define a regular expression to match ISO 8601 date format "YYYY-MM-DD"
 iso8601_date_pattern = r"^\d{4}-\d{2}-\d{2}$"
@@ -12,20 +13,31 @@ controlled_list_df = pd.read_csv(
 # Replace 'column_name' with the actual column name in your CSV
 controlled_list = controlled_list_df['baseMaterialType'].tolist()
 
+# Custom check function for UUID4 format
+# previous check was pa.Check.str_length(36),
+
+
+def check_uuid4(value):
+    try:
+        uuid_obj = uuid.UUID(value, version=4)
+        return True
+    except ValueError:
+        return False
+
+
 # import column from pandera as pa.column
 # strict='filter' will drop columns not in the schema from the validation process
 # add missing columns - will print a return schema that indicates missing columns - with a value of NaN if no default is declared.
 schema = pa.DataFrameSchema(
     {
-        "identifier": pa.Column(str),
-        "baseMaterialName": pa.Column(str),
-        "baseMaterialType": pa.Column(pa.String, checks=pa.Check(lambda s: s.isin(controlled_list)), required=False),
-        "materialChemCID": pa.Column(int, required=False),
-        "externalIdentifierKeys": pa.Column(str, required=False),
-        "externalIdentifierValues": pa.Column(int, required=False),
+        "identifier": pa.Column(str, checks=pa.Check(lambda s: check_uuid4(s))),
+        "materialName": pa.Column(str),
+        "externalIdentifier": pa.Column(dict, required=False),
+        "materialConstituents": pa.Column(list),
+        "combinationPurpose": pa.Column(str, required=False),
         "certification": pa.Column(bool, required=False),
-        "certificationClaims": pa.Column(str, required=False),
-        "manufacturedCountry": pa.Column(str, required=False),
+        "certificationClaims": pa.Column(list, required=False),
+        "manufacturedCountry": pa.Column(int, required=False),
         "updateDate": pa.Column(pa.String, checks=pa.Check.str_matches(iso8601_date_pattern)),
     },
     #   strict='filter',
